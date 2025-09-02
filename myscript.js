d1e1da8ace9857c726834f32b13ab0e6234a8faf
rogue-acid <galyakrinsky@gmail.com>
2025-09-02 13:30:53 -0700

    add: hide paragraphs when h1 is clicked

--- myscript.js
+++ myscript.js
@@ -4,6 +4,10 @@ $(document).ready(function() {
 	});
 	$("h1").click(function() {
 		$("p").addClass("bright");
+		$("p").hide();
 		$("img").addClass("border");
+	});
+	$("h1").click(function(){
+		$("h2, h3").toggleClass(hide());
 	});
 });
